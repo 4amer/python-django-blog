17fcc5ff3449de3b0cca72d0c2bdb546253626e8
4amer <101433018+4amer@users.noreply.github.com>
2022-11-27 18:29:23 +0200

    registration form
    Were added registration and login

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'article'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/pub_article/', views.pub_article, name='pub_article')
+    path('/pub_article/', views.pub_article, name='pub_article'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.register, name="register"),
 ]
--- blog/article/views.py
+++ blog/article/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login as auth_login, logout
+
+from django.contrib import messages
+
 from .models import Article
+from .forms import CreateUserForm
 
 from django.utils import timezone
 
@@ -16,3 +22,38 @@ def pub_article(request):
     a.save()
     return HttpResponseRedirect ( reverse('article:index'))
     
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, "Аккаунт под именем " + user + ", был создан!")
+            return redirect('article:login')
+
+
+    context = {'form': form }
+    return render (request, 'article/register.html', context)
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect('article:index')
+        else:
+            messages.info(request, "Пользователь ИЛИ пароль не верны!")
+
+    context = {}
+    return render (request, 'article/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('article:login')
+
